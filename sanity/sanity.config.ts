e14394f14baaf609c30175cbe050a5ae3985fd12
Arjun Vijay Prakash <137415649+ArjunCodess@users.noreply.github.com>
2024-04-02 03:29:14 +0000

    build error fixed

--- sanity/sanity.config.ts
+++ sanity/sanity.config.ts
@@ -1,6 +1,6 @@
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
-import {visionTool} from '@sanity/vision'
+import {visionTool} from 'sanity/vision'
 import {schemaTypes} from './schemaTypes'
 
 export default defineConfig({
