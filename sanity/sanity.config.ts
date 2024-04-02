357ea142eadf39ae452348ac655a8a2ae793be9c
Arjun Vijay Prakash <137415649+ArjunCodess@users.noreply.github.com>
2024-04-02 03:33:59 +0000

    build eror fix, add sanity folder to .vercelignore

--- sanity/sanity.config.ts
+++ sanity/sanity.config.ts
@@ -1,7 +1,7 @@
-import {defineConfig} from 'sanity'
-import {structureTool} from 'sanity/structure'
-import {visionTool} from 'sanity/vision'
-import {schemaTypes} from './schemaTypes'
+import { defineConfig } from 'sanity'
+import { structureTool } from 'sanity/structure'
+import { visionTool } from '@sanity/vision'
+import { schemaTypes } from './schemaTypes'
 
 export default defineConfig({
   name: 'default',
